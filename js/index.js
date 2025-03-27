72f4be0223875891236bae110b0b4a987b5deb5d
JoachimGautama <joachim.gautama@student.ehb.be>
2025-03-27 15:33:46 +0100

    sync

--- js/index.js
+++ js/index.js
@@ -1,19 +1,17 @@
+import * as Classes from "./class.js";
+
 const items = [];
 
 function init() {
-
+  fetchData();
 }
 
 function fetchData() {
-
+  render();
 }
 
-function onSearch(searchValue) {
+function onSearch(searchValue) {}
 
-}
-
-function render() {
-
-}
+function render() {}
 
 init();
